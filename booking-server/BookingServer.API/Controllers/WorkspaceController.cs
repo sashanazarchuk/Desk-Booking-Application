6e25b82b159e07928126ab4e0e52f9cdfeb4197b
Oleksander Nazarchuk <ss.nazarchuk2@gmail.com>
2025-05-30 01:29:30 +0300

    Add controllers with actions

---
+++ booking-server/BookingServer.API/Controllers/WorkspaceController.cs
@@ -0,0 +1,29 @@
+﻿using BookingServer.Application.Query.Workspace;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkspaceController : ControllerBase
+    {
+
+        private readonly IMediator mediator;
+        
+        public WorkspaceController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var workspace = await mediator.Send(new GetAllWorkspacesQuery());
+
+            return Ok(workspace);
+        }
+    }
+}
