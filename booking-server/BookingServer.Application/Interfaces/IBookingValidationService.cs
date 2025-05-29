498f0b9285ce5e4923a37beea5ab5883874f3a10
Oleksander Nazarchuk <ss.nazarchuk2@gmail.com>
2025-05-30 01:23:10 +0300

    Implement repository interfaces and repository classes

---
+++ booking-server/BookingServer.Application/Interfaces/IBookingValidationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingServer.Application.Interfaces
+{
+    public interface IBookingValidationService
+    {
+        Task<bool> RoomExists(int roomId, CancellationToken ct);
+        Task<bool> IsBookingDurationValid(int roomId, DateTime startDate, DateTime endDate, CancellationToken ct);
+        Task<bool> IsRoomAvailable(int roomId, DateTime startDate, DateTime endDate, CancellationToken ct);
+        Task<bool> IsNotDoubleBookingForSameUser(string email, DateTime startDate, DateTime endDate, CancellationToken ct);
+    }
+}
